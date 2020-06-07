1ff36f54316d9033f9ad1b48433aa6f71892853b
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2020-06-07 20:03:27 +0530

    using recipe service as data service for list. closes #26

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {Recipe} from './recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
+  private recipes: Recipe[] = [
+    new Recipe('Test Recipe 1', 'Test Description', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Test Recipe 2', 'Test Description', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+  ];
+
   constructor() { }
+
+  getRecipes(){
+    return this.recipes.slice();
+  }
 }
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipes-list',
@@ -10,14 +11,12 @@ export class RecipesListComponent implements OnInit {
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('Test Recipe 1', 'Test Description', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Test Recipe 2', 'Test Description', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(r: Recipe) {
