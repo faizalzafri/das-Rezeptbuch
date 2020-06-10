daf5842604b168250dae5c43d66033553f1952be
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2020-06-10 14:08:16 +0530

    data model modified. recipe ow display ingredients. closes #30

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,34 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe 1', 'Test Description', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Test Recipe 2', 'Test Description', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe(
+      'Paprika Chicken',
+      'Straight forward grilled chicken recipe',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F1018451.jpg',
+      [
+        new Ingredient(
+          '(5 pound) whole chicken',
+          1
+        ),
+        new Ingredient(
+          'tablespoons ground paprika',
+          3
+        )
+      ]),
+    new Recipe(
+      'Italian Chicken Marinade',
+      'Delicious way of marinating your chicken',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F665982.jpg',
+      [
+        new Ingredient(
+          'boneless chicken breast halves',
+          4
+        ),
+        new Ingredient(
+          '(16 ounce) bottle Italian-style salad dressing',
+          1
+        )
+      ])
   ];
 
   constructor() { }
